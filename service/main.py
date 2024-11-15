d602cf343a6dcb57faadbd26c7605eb5ab88984a
Randy <ranzhang@cisco.com>
2024-11-15 17:00:45 -0500

    update main.py and api endpoint

--- service/main.py
+++ service/main.py
@@ -5,19 +5,31 @@ app = FastAPI()
 
 # Define the base URLs for the different systems
 
-systems = ["system1", "system2", "system3", "system4"]
+systems = ["fire", "flooding", "earthquake", "burglary", "crime", , "safety", "health"]
 
 @app.get("/api/v1/{system_name}")
 async def get_data(system_name: str):
     if system_name not in systems:
         raise HTTPException(status_code=404, detail="System not found")
 
-    if system_name == "system1":
-        return {"data": "data from system1"}
+    if system_name == "fire":
+        return {"data": "data from fire"}
     
-    if system_name == "system2":
-        return {"data": "data from system2"}
+    if system_name == "flooding":
+        return {"data": "data from flooding"}
     
-    if system_name == "system3":
-        return {"data": "data from system3"}
+    if system_name == "earthquake":
+        return {"data": "data from earthquake"}
+    
+    if system_name == "burglary":
+        return {"data": "data from burglary"}
+    
+    if system_name == "crime":
+        return {"data": "data from crime"}
+    
+    if system_name == "safety":
+        return {"data": "data from safety"}
+    
+    if system_name == "health":
+        return {"data": "data from health"}
     
