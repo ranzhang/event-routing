ae993b7a80df41204c644beb30876f3d61c45d0d
zhangbaiyi <baiyi@vt.edu>
2024-11-15 17:04:52 -0500

    fix comflicts

--- service/main.py
+++ service/main.py
@@ -5,48 +5,30 @@ app = FastAPI()
 
 # Define the base URLs for the different systems
 
-<<<<<<< HEAD
-systems = ["fire", "flooding", "earthquake", "burglary", "crime", , "safety", "health"]
-=======
-systems = ["fire", "burglary", "earthquake", "health"]
->>>>>>> 573254038a5e190fdc6225994dc46c9de42038eb
+systems = ["fire", "flooding", "earthquake", "burglary", "crime",  "safety", "health"]
 
 @app.get("/api/v1/{category}")
 async def get_data(category: str):
     if category not in systems:
         raise HTTPException(status_code=404, detail="System not found")
 
-<<<<<<< HEAD
-    if system_name == "fire":
-        return {"data": "data from fire"}
-    
-    if system_name == "flooding":
-        return {"data": "data from flooding"}
-    
-    if system_name == "earthquake":
-        return {"data": "data from earthquake"}
-    
-    if system_name == "burglary":
-        return {"data": "data from burglary"}
-    
-    if system_name == "crime":
-        return {"data": "data from crime"}
-    
-    if system_name == "safety":
-        return {"data": "data from safety"}
-    
-    if system_name == "health":
-        return {"data": "data from health"}
-=======
     if category == "fire":
         return {"data": "data from fire"}
+    
+    if category == "flooding":
+        return {"data": "data from flooding"}
+    
+    if category == "earthquake":
+        return {"data": "data from earthquake"}
     
     if category == "burglary":
         return {"data": "data from burglary"}
     
-    if category == "earthquake":
-        return {"data": "data from earthquake"}
->>>>>>> 573254038a5e190fdc6225994dc46c9de42038eb
+    if category == "crime":
+        return {"data": "data from crime"}
+    
+    if category == "safety":
+        return {"data": "data from safety"}
     
     if category == "health":
         return {"data": "data from health"}
